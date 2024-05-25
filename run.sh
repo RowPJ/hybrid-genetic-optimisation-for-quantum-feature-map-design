709c43689e328939d6659a5a823a44e3d6fa94c1
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2024-05-25 15:59:31 +0200

    Set python executable for pycall inside run script.

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ source activate
 export LOCAL_WORKER_COUNT=$(nproc)
 
 # start the installed julia with this project and run the experiments
+source ./activate
+PYTHON=$(pwd)/venv/bin/python
 ~/.juliaup/bin/julia --project="." -e "include(\"./src/experiments.jl\"); main()"
