2c2427fe2036d06531135e399c8463a6576e54f2
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2024-05-25 16:49:57 +0200

    Added script to optionally run the code that generates the graphs used in the paper.

---
+++ make_graphs.sh
@@ -0,0 +1,16 @@
+# Before running this file, make sure to first run run.sh to generate
+# its output files.
+
+# activate virtual environment
+source ./activate
+
+# set number of worker processes that julia should start to run
+# experiments. we don't use $(nproc) since higher values can cause
+# segmentation faults.
+#export LOCAL_WORKER_COUNT=$(nproc)
+export LOCAL_WORKER_COUNT=12
+
+# start the installed julia with this project and run the experiments
+source ./activate
+PYTHON=$(pwd)/venv/bin/python
+~/.juliaup/bin/julia --project="." -e "include(\"./src/experiments.jl\"); generate_graphs_new_naming()"
