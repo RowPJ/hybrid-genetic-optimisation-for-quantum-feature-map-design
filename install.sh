3f6d0b6db81122f52f84f38b6095dbeaa5c20953
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2024-05-25 15:33:38 +0200

    Install updated julia in install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ python -m venv venv
 
 source venv/bin/activate
 
+# install julia (on unix OS's)
+curl -fsSL https://install.julialang.org | sh
+
 pip install -r requirements.txt
 
 julia --project="." -e "ENV[\"PYTHON\"]=\"$(pwd)/venv/bin/python\"; import Pkg; Pkg.instantiate(); Pkg.build(\"PyCall\")"
